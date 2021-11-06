debe552d02e3de5ee02a9964fb6b835e0020718c
Oqla Alrefai <2015978054@ses.yu.edu.jo>
2021-11-06 19:18:24 +0200

    challenge 5

--- python/code_challenges/linked_list/linked_list.py
+++ python/code_challenges/linked_list/linked_list.py
@@ -1,12 +1,68 @@
 class LinkedList:
-    """
-    Put docstring here
-    """
-
+    '''this class will link each node together'''
     def __init__(self):
-        # initialization here
-        pass
+        self.head = None
+    def printLinkedList(self):
+        current = self.head
+        if current == None:
+            return 'Linked List is empty'
+        else :
+            while current != None :
+                print(current.data)
+                current = current.next
+    def insert(self, data):
+        newNode = node(data)
+        if(self.head):
+            current = self.head
+            while(current.next):
+                current = current.next
+            current.next = newNode
+        else:
+            self.head = newNode
+    
 
-    def some_method(self):
-        # method body here
-        pass
+
+    def __str__(self):
+        retStr = ""
+
+        current = self.head
+        while(current != None):
+            retStr += f"({current.data}) ->"
+            current = current.next
+
+        
+
+        return f'{retStr} NULL'
+    
+    
+    
+    def includes(self,val): 
+        # Initialize current to head
+        current = self.head
+ 
+        # loop till current not equal to None
+        while current != None:
+            if current.data == val:
+                return f'{True} yes node is exist' # data found
+            current = current.next
+         
+        return f'{False} node not found ' # Data Not found
+
+
+
+class node() :
+    '''this class to create node'''
+    def __init__(self,data) : # its a constructor
+        self.data = data
+        self.next = None
+
+
+
+print(node(5)) #it will create a node but it will print a addres value 
+
+LL = LinkedList() #create instance from class
+LL.insert(3) 
+LL.insert(4)
+LL.insert(5)
+
+print(LL.__str__())
